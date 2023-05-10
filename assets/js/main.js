5cfb29dcffaaaa176f76829b831abfa25d4ff487
natalia vieira <verkappte@hotmail.com>
2023-05-10 15:30:54 -0300

    adiciona Javascript e chama API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-console.log("Sucesso")
+const offset= 0
+const limit = 10
+const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}` 
+
+
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon}</span>
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon}">
+    </div>
+</li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        
+        for (let i = 0; i < pokemons.lenght; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToHTML(pokemon)            
+        }
+})
+    .catch((error) => console.error(error))
+
